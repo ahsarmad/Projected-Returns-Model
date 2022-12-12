9ecc18264cc7ca17dd252877ad56d0a6b86b6701
Sarmad <afzalsarmad77@gmail.com>
2022-12-11 23:21:30 -0500

    parsing search query

--- Trading-Manager/service/ApiService.swift
+++ Trading-Manager/service/ApiService.swift
@@ -8,6 +8,12 @@ import Foundation
 import Combine
 
 struct APISERVICE {
+
+    enum APISERVICEError: Error {
+        case encoding
+        case badRequest
+    }
+
     var API_KEY: String {
         return keys.randomElement() ?? ""
     }
@@ -17,9 +23,14 @@ struct APISERVICE {
     let keys = ["CEQ9UITT7TZZMLIP", "SRYE0D77ZKPUN0BK", "8A82QBTR3WL6E7F7", "U48GGV2ROC3WEG2Q", "J3OI5GIT6TBP9I95", "OJVGNRX2U67PZWO5", "N8K6JFDOU6SY6J2M", "U516F0R3XNPPD5HT", "EO1KVSSMALKZG2RJ", "GOP8SBQWZB89TGGO"]
     
     func fetchSymbolsPublisher(keywords: String) -> AnyPublisher<SearchResults, Error> {
+
+        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { 
+            return Fail(error: APISERVICEError.encoding ).eraseToAnyPublisher() }
+
         let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(API_KEY)"
         
-        let url = URL(string: urlString)!
+        guard let url = URL(string: urlString) else {
+            return Fail(error: APISERVICEError.badRequest).eraseToAnyPublisher()}
         
         return URLSession.shared.dataTaskPublisher(for: url)
             .map({$0.data })
